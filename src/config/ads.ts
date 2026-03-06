45ed3a2059f227bc4381e0cc27cc3f9c7a3b691f
Zied Meddeb <dounzay@gmail.com>
2026-03-06 21:38:37 +0100

    Update ad configurations with actual ad scripts

--- src/config/ads.ts
+++ src/config/ads.ts
@@ -1,7 +1,51 @@
 export const ADS = {
-  topBanner: "PASTE_ADSTERRA_CODE_OR_LINK",
-  middleBanner: "PASTE_ADSTERRA_CODE_OR_LINK",
-  sidebarAd1: "PASTE_ADSTERRA_CODE_OR_LINK",
-  sidebarAd2: "PASTE_ADSTERRA_CODE_OR_LINK",
-  footerBanner: "PASTE_ADSTERRA_CODE_OR_LINK",
+  // Banner 728x90 (Top walla Middle)
+  topBanner: `
+    <script type="text/javascript">
+      atOptions = {
+        'key' : 'b5262129d29f40842476176aedd8035b',
+        'format' : 'iframe',
+        'height' : 90,
+        'width' : 728,
+        'params' : {}
+      };
+    </script>
+    <script type="text/javascript" src="//www.highperformanceformat.com/b5262129d29f40842476176aedd8035b/invoke.js"></script>
+  `,
+
+  // Banner 468x60
+  middleBanner: `
+    <script type="text/javascript">
+      atOptions = {
+        'key' : '068c2bb993cf5727eb61b719f35a5fd5',
+        'format' : 'iframe',
+        'height' : 60,
+        'width' : 468,
+        'params' : {}
+      };
+    </script>
+    <script type="text/javascript" src="//www.highperformanceformat.com/068c2bb993cf5727eb61b719f35a5fd5/invoke.js"></script>
+  `,
+
+  // Sidebar 160x600
+  sidebarAd1: `
+    <script type="text/javascript">
+      atOptions = {
+        'key' : '818f4bad5f670e1e6a184b2928a959fa',
+        'format' : 'iframe',
+        'height' : 600,
+        'width' : 160,
+        'params' : {}
+      };
+    </script>
+    <script type="text/javascript" src="//www.highperformanceformat.com/818f4bad5f670e1e6a184b2928a959fa/invoke.js"></script>
+  `,
+
+  // Native Banner (El 4 tsawer elli ijiw m3a ba3dh-hom)
+  footerBanner: `
+    <script async="async" data-cfasync="false" src="//heavinessslight.com/dd0ed183c97a13585589697f3ce39af/invoke.js"></script>
+    <div id="container-dd0ed183c97a13585589697f3ce39af"></div>
+  `,
+  
+  sidebarAd2: "PASTE_ADSTERRA_CODE_OR_LINK"
 };
