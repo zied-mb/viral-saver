77a5e6351d676943ffa0307445dff34e10770370
Zied Meddeb <dounzay@gmail.com>
2026-03-06 08:25:04 +0100

    Replace ad scripts with Adsterra links
    Updated ad scripts to use Adsterra service.

--- src/config/ads.ts
+++ src/config/ads.ts
@@ -1,56 +1,7 @@
 export const ADS = {
-  topBanner: `
-    <script type="text/javascript">
-      atOptions = {
-        'key' : 'b5262129d29f40642476176aedd8035b',
-        'format' : 'iframe',
-        'height' : 90,
-        'width' : 728,
-        'params' : {}
-      };
-    </script>
-    <script type="text/javascript" src="https://www.highperformanceformat.com/b5262129d29f40642476176aedd8035b/invoke.js"></script>
-  `,
-  
-  middleBanner:   `<script async="async" data-cfasync="false" src="https://pl28855346.effectivegatecpm.com/dd0ed183c97a135855896979f3ce39af/invoke.js"></script><div id="container-dd0ed183c97a135855896979f3ce39af"></div>`,
-
-  
-  sidebarAd1: `
-    <script type="text/javascript">
-      atOptions = {
-        'key' : '818f4bad5f670e1e6a184b2928a959fa',
-        'format' : 'iframe',
-        'height' : 600,
-        'width' : 160,
-        'params' : {}
-      };
-    </script>
-    <script type="text/javascript" src="https://www.highperformanceformat.com/818f4bad5f670e1e6a184b2928a959fa/invoke.js"></script>
-  `,
-
-  
-  sidebarAd2: `
-    <script type="text/javascript">
-      atOptions = {
-        'key' : 'b5262129d29f40642476176aedd8035b',
-        'format' : 'iframe',
-        'height' : 90,
-        'width' : 728,
-        'params' : {}
-      };
-    </script>
-    <script type="text/javascript" src="https://www.highperformanceformat.com/b5262129d29f40642476176aedd8035b/invoke.js"></script>
-  `,
-  footerBanner: `
-    <script type="text/javascript">
-      atOptions = {
-        'key' : '068c2bb993cf5727eb61b71f9f35a5f5',
-        'format' : 'iframe',
-        'height' : 60,
-        'width' : 468,
-        'params' : {}
-      };
-    </script>
-    <script type="text/javascript" src="https://www.highperformanceformat.com/068c2bb993cf5727eb61b71f9f35a5f5/invoke.js"></script>
-  `,
+  topBanner: `<script type="text/javascript" src="https://ad.adsterra.com/show.js?id=28753765"></script>`,
+  middleBanner: `<script type="text/javascript" src="https://ad.adsterra.com/show.js?id=28754928"></script>`,
+  sidebarAd1: `<script type="text/javascript" src="https://ad.adsterra.com/show.js?id=28754985"></script>`,
+  sidebarAd2: `<script type="text/javascript" src="https://ad.adsterra.com/show.js?id=28754847"></script>`,
+  footerBanner: `<script type="text/javascript" src="https://ad.adsterra.com/show.js?id=28754958"></script>`,
 };
