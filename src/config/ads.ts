3214085e8580e2d8251fcabc899f28f090931aba
Zied Meddeb <dounzay@gmail.com>
2026-03-06 07:59:12 +0100

    ads

--- src/config/ads.ts
+++ src/config/ads.ts
@@ -1,7 +1,56 @@
 export const ADS = {
-  topBanner: "PASTE_ADSTERRA_CODE_OR_LINK",
-  middleBanner: "PASTE_ADSTERRA_CODE_OR_LINK",
-  sidebarAd1: "PASTE_ADSTERRA_CODE_OR_LINK",
-  sidebarAd2: "PASTE_ADSTERRA_CODE_OR_LINK",
-  footerBanner: "PASTE_ADSTERRA_CODE_OR_LINK",
+  topBanner: `
+    <script type="text/javascript">
+      atOptions = {
+        'key' : 'b5262129d29f40642476176aedd8035b',
+        'format' : 'iframe',
+        'height' : 90,
+        'width' : 728,
+        'params' : {}
+      };
+    </script>
+    <script type="text/javascript" src="https://www.highperformanceformat.com/b5262129d29f40642476176aedd8035b/invoke.js"></script>
+  `,
+  
+  middleBanner:   `<script async="async" data-cfasync="false" src="https://pl28855346.effectivegatecpm.com/dd0ed183c97a135855896979f3ce39af/invoke.js"></script><div id="container-dd0ed183c97a135855896979f3ce39af"></div>`,
+
+  
+  sidebarAd1: `
+    <script type="text/javascript">
+      atOptions = {
+        'key' : '818f4bad5f670e1e6a184b2928a959fa',
+        'format' : 'iframe',
+        'height' : 600,
+        'width' : 160,
+        'params' : {}
+      };
+    </script>
+    <script type="text/javascript" src="https://www.highperformanceformat.com/818f4bad5f670e1e6a184b2928a959fa/invoke.js"></script>
+  `,
+
+  
+  sidebarAd2: `
+    <script type="text/javascript">
+      atOptions = {
+        'key' : 'b5262129d29f40642476176aedd8035b',
+        'format' : 'iframe',
+        'height' : 90,
+        'width' : 728,
+        'params' : {}
+      };
+    </script>
+    <script type="text/javascript" src="https://www.highperformanceformat.com/b5262129d29f40642476176aedd8035b/invoke.js"></script>
+  `,
+  footerBanner: `
+    <script type="text/javascript">
+      atOptions = {
+        'key' : '068c2bb993cf5727eb61b71f9f35a5f5',
+        'format' : 'iframe',
+        'height' : 60,
+        'width' : 468,
+        'params' : {}
+      };
+    </script>
+    <script type="text/javascript" src="https://www.highperformanceformat.com/068c2bb993cf5727eb61b71f9f35a5f5/invoke.js"></script>
+  `,
 };
