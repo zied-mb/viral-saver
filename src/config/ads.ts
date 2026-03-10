981e88e6e8cb18c6ff634eb735e8c9d6a81bcec7
Zied Meddeb <dounzay@gmail.com>
2026-03-10 06:24:38 +0100

    Update ads.ts

--- src/config/ads.ts
+++ src/config/ads.ts
@@ -1,7 +1,13 @@
 export const ADS = {
+  // MDB Collection 
   topBanner: "", 
-  sidebarAd1: String(import.meta.env.VITE_HILLTOP_SIDEBAR_ID || ""),
-  sidebarAd2: String(import.meta.env.VITE_HILLTOP_RESULT_ID || ""),
-  middleBanner: String(import.meta.env.VITE_ADS_MIDDLE_BANNER || ""), 
-  footerBanner: String(import.meta.env.VITE_ADS_FOOTER_BANNER || ""),
+
+  // Popunder 
+  downloaderPop: String(import.meta.env.VITE_HILLTOP_POP_ID || "").trim(),
+
+  // Banner
+  sidebarAd1: String(import.meta.env.VITE_HILLTOP_SIDEBAR_ID || "").trim(),
+  sidebarAd2: String(import.meta.env.VITE_HILLTOP_RESULT_ID || "").trim(),
+  middleBanner: String(import.meta.env.VITE_HILLTOP_MIDDLE_ID || "").trim(), 
+  footerBanner: String(import.meta.env.VITE_HILLTOP_FOOTER_ID || "").trim(),
 };
