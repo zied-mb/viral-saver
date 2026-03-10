107bc0889e378da20398c9abb09f8c99c11e0817
Zied Meddeb <dounzay@gmail.com>
2026-03-10 01:11:26 +0100

    Update ads.ts

--- src/config/ads.ts
+++ src/config/ads.ts
@@ -2,5 +2,7 @@ export const ADS = {
   topBanner: "",
   sidebarAd1: import.meta.env.VITE_HILLTOP_SIDEBAR_ID,
   sidebarAd2: import.meta.env.VITE_HILLTOP_RESULT_ID,
-  middleFooterBanner: import.meta.env.VITE_ADS_MIDDLE_BANNER || import.meta.env.VITE_ADS_FOOTER_BANNER,
+
+  middleBanner: import.meta.env.VITE_ADS_MIDDLE_BANNER, 
+  footerBanner: import.meta.env.VITE_ADS_FOOTER_BANNER,
 };
