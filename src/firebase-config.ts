9458da5988e8356d214cad784515c760d94eb3f9
Zied Meddeb <dounzay@gmail.com>
2026-03-15 02:15:04 +0100

    Update firebase-config.ts

--- src/firebase-config.ts
+++ src/firebase-config.ts
@@ -6,7 +6,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyA5whf3LBXM--Ma4zARDbdKYXx7LvEkyfc",
   authDomain: "viral-saver-fd922.firebaseapp.com",
   projectId: "viral-saver-fd922",
-  databaseURL: "https://viral-saver-fd922-default-rtdb.firebaseio.com",
+  databaseURL: "https://viral-saver-fd922-default-rtdb.europe-west1.firebasedatabase.app", 
   storageBucket: "viral-saver-fd922.firebasestorage.app",
   messagingSenderId: "755065276076",
   appId: "1:755065276076:web:221b63fa8a0fc475c98109",
